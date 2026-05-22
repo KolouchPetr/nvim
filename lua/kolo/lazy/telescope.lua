81d79467202125a6d432de1674de8a3ea49fec54
kolouch <petr.kolouch@fnusa.cz>
2026-05-22 14:11:07 +0200

    updated config

--- lua/kolo/lazy/telescope.lua
+++ lua/kolo/lazy/telescope.lua
@@ -5,9 +5,10 @@ return {
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
+    cmd = "Telescope",
         keys = {
         { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-        { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
+        { '<leader>fs', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
         { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find buffers' },
         { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Help tags' },
     },
